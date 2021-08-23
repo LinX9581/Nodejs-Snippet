3fb01e3ca4b9136afcc44f049e4247283de5eb3f
linx <linx9581@gmail.com>
2021-08-23 14:59:43 +0800

    asdfg

--- shell/wnmp.sh
+++ shell/wnmp.sh
@@ -12,9 +12,10 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt-get install ca-certificates apt-transport-https -y
 #sudo apt-get install php7.2 php7.2-fpm php7.2-cli php7.2-common php7.2-opcache php7.2-mbstring php7.2-mysql php7.2-zip php7.2-xml php7.2-curl php7.2-gd php7.2-bz2 git nginx mariadb-server mariadb-client -y
-sudo apt install php7.4 php7.4-fpm php7.4-cli php7.4-common php7.4-xmlrpc php7.4-opcache php7.4-mysql php7.4-gd php7.4-zip php7.4-xml php7.4-cli php7.4-dev php7.4-imap php7.4-soap php7.4-intl php7.4-curl php7.4-mbstring git nginx mariadb-server mariadb-client -y
-sudo systemctl start php7.4-fpm nginx mariadb
-sudo systemctl enable php7.4-fpm nginx mariadb
+# sudo apt install php7.4 php7.4-fpm php7.4-cli php7.4-common php7.4-xmlrpc php7.4-opcache php7.4-mysql php7.4-gd php7.4-zip php7.4-xml php7.4-cli php7.4-dev php7.4-imap php7.4-soap php7.4-intl php7.4-curl php7.4-mbstring git nginx mariadb-server mariadb-client -y
+sudo apt install php8.0-fpm php8.0-common php8.0-mysql php8.0-gmp php8.0-curl php8.0-intl php8.0-mbstring php8.0-xmlrpc php8.0-gd php8.0-xml php8.0-cli php8.0-zip git nginx mariadb-server mariadb-client -y
+sudo systemctl start php8.0-fpm nginx mariadb
+sudo systemctl enable php8.0-fpm nginx mariadb
 cd /tmp
 wget http://wordpress.org/latest.tar.gz
 sudo tar -xvzf latest.tar.gz -C /var/www/html
